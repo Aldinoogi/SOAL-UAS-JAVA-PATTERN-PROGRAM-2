f303a051eb56f112647a58282db4b4fab9dcde7e
Aldinoogi <93021737+Aldinoogi@users.noreply.github.com>
2022-01-22 10:16:43 +0700

    Pattern 2

---
+++ src/Pattern2.java
@@ -0,0 +1,15 @@
+
+public class Pattern2 {
+	public static void main (String[]args) {
+		int row;
+		int star = 0;
+
+		for (row = 5; row >= 1; row--) {
+			for (star = 1; star<=row; star++) {
+				System.out.print ("* ");
+			}
+
+			System.out.println();
+		}
+	}
+}
